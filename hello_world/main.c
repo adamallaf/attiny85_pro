b75adbeac121e3f4ac6a5274e6d63ff4be53e1b2
Adam Allaf <adam.allaf@hotmail.com>
2024-07-26 18:40:44 +0200

    Added clang format settings and reformated code

--- hello_world/main.c
+++ hello_world/main.c
@@ -1,32 +1,28 @@
-#include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/io.h>
 #include <avr/power.h>
 #include <avr/wdt.h>
 
+int main() {
+	cli(); // disable interrupts
+	// WDT disable
+	MCUSR &= ~(1 << WDRF);
+	WDTCR |= ((1 << WDCE) | (1 << WDE)); // change enable
+	WDTCR = 0x00;
 
-int main(){
-    cli();                  // disable interrupts
-    // WDT disable
-    MCUSR &= ~(1 << WDRF);
-    WDTCR |= ((1 << WDCE) | (1 << WDE));  // change enable
-    WDTCR = 0x00;
+	clock_prescale_set(clock_div_1);
 
-    clock_prescale_set(clock_div_1);
+	// WDT enable
+	WDTCR |= ((1 << WDCE) | (1 << WDE));             // change enable
+	WDTCR = (1 << WDIE) | (1 << WDP1) | (1 << WDP2); // int enabled & 1s timeout
 
-    // WDT enable
-    WDTCR |= ((1 << WDCE) | (1 << WDE));  // change enable
-    WDTCR = (1 << WDIE) | (1 << WDP1) | (1 << WDP2);  // int enabled & 1s timeout
+	DDRB = 0x00;
+	DDRB |= (1 << PB2); // PB2 as outputs
 
-    DDRB = 0x00;
-    DDRB |= (1 << PB2);     // PB2 as outputs
-
-    sei();                  // enable interrupts
-    while(1){
-
-    }
-    return 0;
+	sei(); // enable interrupts
+	while ( 1 ) {
+	}
+	return 0;
 }
 
-ISR(WDT_vect){
-    PORTB ^= (1 << PB2);
-}
+ISR(WDT_vect) { PORTB ^= (1 << PB2); }
